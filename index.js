18164ac1b7797f14f996d4c2978dedd8ade7dc19
Aakash Solanki <aakashsolanki@Aakashs-MacBook-Air.local>
2025-07-20 12:23:03 +0530

    add vercel api

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',                    
-    'https://next-property-9x73.vercel.app'    
+    'https://next-property-25z6.vercel.app/'    
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
